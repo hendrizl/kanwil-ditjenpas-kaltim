12e532be0e1a1309adcc9538c35b87ef02db658f
Hendrizal <hendri.zzzzz@gmail.com>
2026-08-30 19:59:44 +0800

    chore: setup next.config for github pages and update dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+
+  basePath: "/kanwil-ditjenpas-kaltim",
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
